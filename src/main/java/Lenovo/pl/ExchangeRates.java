9bf314fd88481d8aefab241ee5e7ef7acc9dd017
LENOVO <dawidPn58@gmail.com>
2025-01-15 22:01:24 +0100

    second commit

---
+++ src/main/java/Lenovo/pl/ExchangeRates.java
@@ -0,0 +1,24 @@
+package Lenovo.pl;
+
+import java.util.Map;
+
+public class ExchangeRates {
+    private String base;
+    private Map<String, Double> rates;
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public Map<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+}
--- src/main/java/Lenovo/pl/Main.java
+++ src/main/java/Lenovo/pl/Main.java
@@ -1,7 +1,60 @@
 package Lenovo.pl;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+import java.util.Scanner;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Currency to convert: ");
+        String fromCurrency = scanner.nextLine().toUpperCase();
+
+        System.out.println("Target currency: ");
+        String toCurrency = scanner.nextLine().toUpperCase();
+
+        System.out.println("quota: ");
+        double quota = scanner.nextDouble();
+
+        try {
+            Properties properties = new Properties();
+            FileInputStream configFile = new FileInputStream("config.properties");
+            properties.load(configFile);
+            configFile.close();
+
+            String apiUrl = properties.getProperty("api.url");
+
+            String requestUrl = String.format("%s/%s", apiUrl, fromCurrency);
+            URL url = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            Scanner apiScanner = new Scanner(connection.getInputStream());
+            StringBuilder sb = new StringBuilder();
+            while (apiScanner.hasNext()){
+                sb.append(apiScanner.nextLine());
+            }
+            apiScanner.close();
+
+            ObjectMapper mapper = new ObjectMapper();
+            ExchangeRates rates = mapper.readValue(sb.toString(), ExchangeRates.class);
+
+            Double exchangeRate = rates.getRates().get(toCurrency);
+            if (exchangeRate == null){
+                System.out.println("Wrong currency");
+            }
+            else {
+                double convertQuota = quota * exchangeRate;
+                System.out.println("Result: %.2f %s%n" + convertQuota + toCurrency);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
